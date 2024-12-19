d29e5b3fee49c3b8f278cccbc52556b05b8503cf
laurent <laurent.neveux@oclock.io>
2024-12-19 10:28:31 +0100

    update lists

--- assets/js/modules/lists/api.lists.module.js
+++ assets/js/modules/lists/api.lists.module.js
@@ -9,6 +9,9 @@ async function getLists() {
     return lists;
 }
 
+// * on doit récupérer les infos d'un formulaire
+// * on doit envoyer les données du formulaire à l'API pour créer une nouvelle liste
+// * quand la liste est créée, on doit l'ajouter sur le DOM
 async function createList(event) {
     // * empêche le comportement par défaut d'un élément HTML : dans notre cas avec un formulaire, on empêche la page de se recharger
     event.preventDefault();
@@ -40,4 +43,43 @@ async function createList(event) {
     hideModals();
 }
 
-export { getLists, createList };
+async function updateList(event) {
+    event.preventDefault();
+
+    // on se répète, il faudrait extraire une fonction qui récupère les données d'un formulaire
+    const form = event.target;
+    // * on utilise un objet FormData pour récupérer les infos du formulaire
+    const formData = new FormData(form);
+    //   on convertit l'objet formdata en objet litéral
+    const dataObj = Object.fromEntries(formData);
+
+    const response = await fetch(
+        `${config.base_url}/lists/${dataObj.list_id}`,
+        {
+            method: 'PATCH',
+            // * ce header est obligatoire pour que l'API comprenne le type de données qu'on lui envoie
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // * on convertit l'objet en string pour pouvoir l'envoyer dans les tuyaux http
+            body: JSON.stringify(dataObj),
+        }
+    );
+
+    if (response.ok) {
+        const updatedList = await response.json();
+
+        // * mise à jour de la liste sur le DOM
+        const listToUpdate = document.querySelector(
+            `[data-list-id="${updatedList.id}"]`
+        );
+
+        listToUpdate.querySelector('h2').textContent = updatedList.title;
+
+        form.reset();
+    }
+
+    hideModals();
+}
+
+export { getLists, createList, updateList };
--- assets/js/modules/lists/lists.module.js
+++ assets/js/modules/lists/lists.module.js
@@ -7,6 +7,22 @@ function showAddListModal() {
     //   modal.className = 'is-active';
 }
 
+function showEditListModal(list) {
+    const modal = document.querySelector('#editListModal');
+    modal.classList.add('is-active');
+
+    //  on sélectionne l'input caché et on lui donne la valeur de l'id de la liste que l'on vient de cliquer
+    modal.querySelector('input[type=hidden]').value = list.id;
+
+    // on récupère les infos qui sont sur le DOM car elles sont dynamiques, et donc ça dynamise la valeur inpout automatiquement
+    const listToUpdate = document.querySelector(`[data-list-id="${list.id}"]`);
+    //  on sélectionne l'input title et on lui donne la valeur du titre de la liste que l'on vient de cliquer
+    const inputTitle = modal.querySelector('input[name=title]');
+    inputTitle.value = listToUpdate.querySelector('h2').textContent;
+
+    // inputTitle.setAttribute('placeholder', 'un trux');
+}
+
 /**
  * * Cette fonction clone un template du DOM et sert à afficher nos listes sur la page
  * @param {object} list
@@ -21,10 +37,21 @@ function makeList(list) {
     const input = clone.querySelector('[name="list_id"]');
     input.value = list.id;
 
+    const editBtn = clone.querySelector('.js-edit-btn');
+    editBtn.addEventListener('click', () => showEditListModal(list));
+
     // l'attribut HTML custom data-* permet de stocker dans le html des infos dont on aurait besoin pour faire des URL, des formulaires etc etc : la fin de l'histoire
     //   data-*
-    //   const listDataAttr = clone.dataset.listId;
     //   console.log(listDataAttr);
+    const panel = clone.querySelector('.panel');
+
+    // deux façons de faire pour mettre à jour l'attribut data-list-id dans le HTML
+    // dataset :
+    //  panel.dataset.listId = list.id;
+    // la méthode setAttribute
+    panel.setAttribute('data-list-id', list.id);
+    // la méthode setAttribute sert à mettre en place tous type d'attributs
+    // panel.setAttribute('class', `js-${list.id}`);
 
     document.getElementById('lists-container').appendChild(clone);
 }
